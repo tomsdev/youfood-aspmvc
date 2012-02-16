a4ebd3f5335c9ca48e49804fd679a4787258e2d5
tomsdev <74759@supinfo.com>
2012-02-16 10:12:02 +0100

    first

---
+++ tmf/ApplicationHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Web.Mvc;
+namespace tmf
+{
+    public static class ApplicationHelper
+    {
+        public static bool HasMobileSpecificViews
+        {
+
+            get
+            {
+                bool configCheck;
+                bool.TryParse(ConfigurationManager.AppSettings["HasMobileSpecificViews"], out configCheck);
+                return configCheck;
+
+            }
+        }
+        /// <summary>
+        /// Used to enable debugging using alternative devices
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static bool IsSupportedMobileDevice(this HttpRequestBase request)
+        {
+
+            bool isMobile = request.Browser.IsMobileDevice;
+            string userAgent = request.UserAgent.ToLowerInvariant();
+
+            isMobile = isMobile || (userAgent.Contains("iphone")
+                || userAgent.Contains("blackberry")
+                || userAgent.Contains("mobile")
+                || userAgent.Contains("windows ce")
+                || userAgent.Contains("opera mini")
+                || userAgent.Contains("palm")
+               || userAgent.Contains("fennec")
+               || userAgent.Contains("adobeair")
+               || userAgent.Contains("ripple")
+
+                );
+            return isMobile;
+
+        }
+
+        public static string MobileViewsDirectoryName
+        {
+            get
+            {
+                string directoryName = ConfigurationManager.AppSettings["MobileViewsDirectoryName"];
+                return !string.IsNullOrEmpty(directoryName) ? String.Format("{0}/", directoryName) : string.Empty;
+            }
+        }
+
+    }
+}
---
+++ tmf/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ElegantHTML5MobileReady")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Web Divisions")]
+[assembly: AssemblyProduct("ElegantHTML5MobileReady")]
+[assembly: AssemblyCopyright("Copyright © Web Divisions 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f53eac07-eb23-4719-8981-30c55254cca0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
