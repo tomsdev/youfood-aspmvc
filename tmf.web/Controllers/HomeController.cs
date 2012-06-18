b18a02098711d3885a07d007d9e277caaa8a3bcc
Thomas LAURENT <thomas.laurent@supinfo.com>
2012-06-18 15:51:14 +0200

    AJout page d'accueil avec selection du restaurant (stocker en session)

--- tmf.web/Controllers/HomeController.cs
+++ tmf.web/Controllers/HomeController.cs
@@ -3,21 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using tmf.entities;
 using tmf.web.Models;
+using tmf.web.ViewModels;
 
 namespace tmf.web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IWaiterRepository waiterRepository;
+		private readonly IRestaurantRepository restaurantRepository;
+		private readonly IOrderRepository orderRepository;
+
+		// If you are using Dependency Injection, you can delete the following constructor
+        public HomeController()
+            : this(new WaiterRepository(), new RestaurantRepository(), new OrderRepository())
+        {
+        }
+
+        public HomeController(IWaiterRepository waiterRepository, IRestaurantRepository restaurantRepository, IOrderRepository orderRepository)
+        {
+			this.waiterRepository = waiterRepository;
+			this.restaurantRepository = restaurantRepository;
+			this.orderRepository = orderRepository;
+        }
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to kick-start your ASP.NET MVC application.";
-
-            //var ctx = new ApplicationServices();
-            //var list = ctx.Campus.ToList();
-            //ViewBag.Campus = list;
+            ViewBag.PossibleRestaurants = restaurantRepository.All;
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(IndexViewModel indexViewModel)
+        {
+            var restaurant = restaurantRepository.Find(indexViewModel.RestaurantId);
+
+            Session["restaurant"] = restaurant;
+
+            //exemple pour recup le resto:
+            //var monResto = Session["restaurant"] as Restaurant;
+
+            return RedirectToAction("About");
         }
 
         public PartialViewResult Result()
